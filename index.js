5958d621d842e9d05bba9811c8bf217bbf687e55
qafrankly <31970630+qafrankly@users.noreply.github.com>
2017-10-18 19:58:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import ReactDOM from 'react-websocket'
+import ReactDOM from 'react-dom'
 
 class RandomTest extends Component {
   static PropTypes = {
